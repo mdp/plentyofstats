9a96502b54a2df936b94970d31dca78d5f51f0e7
Mark Percival <mark@mpercival.com>
2009-01-18 13:35:11 -0500

    cleaning up

--- specs/regex.rb
+++
@@ -1,4 +0,0 @@
-results = "blah Results 0 to 20 out of 298 results are shown below. and then some"
-
-results.match(/Results \d+ to \d+ out of (\d+) results are shown below/)
-p $1
--- specs/yaml_test.rb
+++
@@ -1,4 +0,0 @@
-require 'yaml'
-
-readme = YAML::load( File.open( 'options.yaml' ) )
-p readme['options']
